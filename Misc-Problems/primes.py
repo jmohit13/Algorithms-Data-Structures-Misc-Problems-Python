05f18782bb944c8cb4c837efe620692a0eed4580
Jain Mohit <reachoutmohit@gmail.com>
2018-12-06 16:10:32 +0530

    updated Primes

--- Misc-Problems/primes.py
+++ Misc-Problems/primes.py
@@ -7,13 +7,13 @@ def isPrime(x):
         x : integer
         
     """
-    rm = range(1,10); primeCount = 0
+    numRange = range(1,10); primeCount = 0
     
     if x > 0:
         if x == 1:
             print("1 is not prime number")
         else:
-            for i in rm:
+            for i in numRange:
                 if x % i == 0:
                     primeCount += 1;
             if primeCount == 2:
