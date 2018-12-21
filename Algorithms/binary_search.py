353abdfce722b6e11919f880785d94866d4f0ca9
Jain Mohit <reachoutmohit@gmail.com>
2018-12-21 10:07:31 +0530

    Added binary search

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,35 @@
+# Binary search
+# 
+# Best-case performance   O(1)
+# Average performance O(log n)
+# Worst-case space complexity O(1)
+
+import math
+
+def do_the_binary_search(l,item):
+    """
+    INPUT
+    ---------
+        l : list
+        item : item to search in the list
+    
+    RETURN
+    ---------
+        itemFound : Flag indicating True or False
+    """
+   
+    l.sort() # sort the list
+    first = 0; last = len(l) - 1
+    itemFound = False
+    
+    while(first <= last and itemFound is False):
+        midpoint = math.floor((first + last)/2)
+        if item == l[midpoint]:
+            itemFound = True
+        else:
+            if item < l[midpoint]:
+                last = midpoint - 1 # Update the last pointer
+            else:
+                first = midpoint + 1 # Update the first pointer
+                
+    return itemFound
