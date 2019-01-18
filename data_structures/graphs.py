cd36c3c304d4457e4816de84bd2fcd102173bf5e
Mohit Jain <reachoutmohit@gmail.com>
2019-01-19 00:50:49 +0530

    Expanded Vertex methods & Added Unit test

--- data_structures/graphs.py
+++ data_structures/graphs.py
@@ -1,4 +1,6 @@
-
+import sys
+import os
+import unittest
 
 class Vertex:
     """
@@ -8,10 +10,31 @@ class Vertex:
     def __init__(self,key):
         self.id = key
         self.connectedTo = {}
+        self.dist = sys.maxsize
+        self.color = 'white'
+        self.pred = None # predecessor
         
     def addNeighbor(self, nbr, weight=0):
         self.connectedTo[nbr] = weight
     
+    def setDistance(self,d):
+        self.dist = d
+        
+    def setColor(self,color):
+        self.color = color
+        
+    def setPred(self,p):
+        self.pred = p
+        
+    def getDistance(self):
+        return self.dist
+    
+    def getColor(self):
+        return self.color
+    
+    def getPred(self):
+        return self.pred
+
     def __str__(self):
         return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
         
@@ -60,28 +83,45 @@ class Graph:
     def __iter__(self):
         return iter(self.vertList.values())
     
-    
+
+class adjGraphTest(unittest.TestCase):
+    def setUp(self):
+        self.tempGraph = Graph()
+
+    def testMakeGraph(self):
+        l = [(0,1),(0,2),(1,2),(2,3),(3,4),(3,5),(4,0)]
+        for i in l:
+            v1, v2 = i[0], i[1]
+            self.tempGraph.addEdge(v1,v2)            
+        for i in self.tempGraph:
+            adj = i.getConnections()
+            for k in adj:
+                print(str(i.id) + ' connectedTo: ' + str([x.id for x in i.connectedTo]) + ' ' 
+                    + str(k.id) + ' connectedTo: ' + str([x.id for x in k.connectedTo]))
+
+
 if __name__ == "__main__":
+    unittest.main()
 
-    g = Graph()
-    for i in range(6):
-        g.addVertex(i)
+    # g = Graph()
+    # for i in range(6):
+    #     g.addVertex(i)
 
-    print("Vertices List :",g.vertList)
+    # print("Vertices List :",g.vertList)
 
-    g.addEdge(0,1,5)
-    g.addEdge(0,2,2)
-    g.addEdge(1,2,3)
-    g.addEdge(2,3,9)
-    g.addEdge(3,4,7)
-    g.addEdge(3,5,3)
-    g.addEdge(4,0,1)
-    g.addEdge(5,4,8)
-    g.addEdge(5,2,1)
+    # g.addEdge(0,1,5)
+    # g.addEdge(0,2,2)
+    # g.addEdge(1,2,3)
+    # g.addEdge(2,3,9)
+    # g.addEdge(3,4,7)
+    # g.addEdge(3,5,3)
+    # g.addEdge(4,0,1)
+    # g.addEdge(5,4,8)
+    # g.addEdge(5,2,1)
 
-    for v in g:
-        for w in v.getConnections():
-            print(v.getId(), w.getId())
+    # for v in g:
+    #     for w in v.getConnections():
+    #         print(v.getId(), w.getId())
             
-    print("Vertices List :",g.getVertices())
-    print("Number of vertices :",g.numVertices)
+    # print("Vertices List :",g.getVertices())
+    # print("Number of vertices :",g.numVertices)
